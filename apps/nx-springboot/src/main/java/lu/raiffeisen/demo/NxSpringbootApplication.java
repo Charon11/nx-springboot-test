b9c3bbec18124af20a395d7f0df95bc56010a43e
Renaud Chardin <chardin.r@sfeir.com>
2023-10-31 15:19:23 +0100

    feat: add spring boot project & build without embedded mvn

---
+++ apps/nx-springboot/src/main/java/lu/raiffeisen/demo/NxSpringbootApplication.java
@@ -0,0 +1,13 @@
+package lu.raiffeisen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NxSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NxSpringbootApplication.class, args);
+	}
+
+}
